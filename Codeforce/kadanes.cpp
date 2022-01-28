634b38335e42111ee6586ad2d9a5ad9d5035e14c
007hyno <parasrawat05@gmail.com>
2022-01-28 18:02:55 +0530

    Kadane's algoritm

---
+++ Codeforce/kadanes.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+long long maxSum(int *a, int n){
+        
+        int m = -999999,c=0;
+        for(int i =0;i<n;i++){
+            c+=a[i];
+            if(c>m){
+                m=c;
+            }
+            // if(c<0){
+            //     c=0;
+            // }
+        }
+        return m;
+    }
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int nn=maxSum(a,n);
+    cout<<nn<<endl;
+    return 0;
+}
